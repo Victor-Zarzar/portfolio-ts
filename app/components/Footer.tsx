57540895f4cdc94f075df8c878d8626bcaab16c3
victorzarzar <victorzarzar58@gmail.com>
2023-12-31 13:30:20 -0300

    implement page about and create route, and corrections in navbar, and footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Footer: React.FC = () => {
+
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <div className="mt-10 h-24 p-8 pb-10">
+            <footer className="mx-auto flex max-w-4xl flex-row">
+                <ul className="flex items-center mx-auto flex-wrap mb-6 md:mb-0">
+                    <li><p className="md:text-xs lg:text-lg font-normal ttext-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400 hover:underline mr-4 md:mr-6"> © {currentYear} Victor Zarzar. All rights reserved.</p></li>
+                </ul>
+            </footer>
+        </div>
+    )
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { Bars3BottomRightIcon, XMarkIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+import Image from "next/image";
+
+interface NavbarProps {
+  theme: string;
+  setTheme: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ theme, setTheme }) => {
+  const Links = [
+    { name: "Home", link: "portfolio" },
+    { name: "About me", link: "about" },
+    { name: "Projects", link: "projects" },
+  ];
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="w-full max-w-7xl">
+      <div className="md:flex items-center justify-between py-4 md:px-10 px-7">
+        <div className="font-bold text-2xl cursor-pointer flex items-center gap-1">
+          <span className="text-xl text-gray-400 hover:text-white focus:outline-none pl-6 md:pl-20">Victor Zarzar</span>
+          <Image
+            src="/src/app/assets/webdeveloper.gif"
+            alt="webdeveloper"
+            width={12}
+            height={12}
+            priority
+          />
+        </div>
+        <div
+          onClick={() => setOpen(!open)}
+          className="absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7 text-gray-400">
+          {open ? <XMarkIcon /> : <Bars3BottomRightIcon />}
+        </div>
+        <ul
+          className={`md:flex md:items-center md:pb-0 absolute md:static  md:z-auto z-10 left-0 w-full md:w-auto mt-4 md:mt-0 md:pl-0 pl-9 sm:bg-transparent transition-all duration-500 ease-in ${open ? "top-12" : "top-[-490px]"}`}>
+          {Links.map((link) => (
+            <li className="md:ml-8 md:my-0 my-3 font-semibold" key={link.name}>
+              <Link href={`/${link.link}`} passHref>
+                <div className="text-xl text-gray-400 hover:text-white focus:outline-none hover:text-primary duration-500">
+                  {link.name}
+                </div>
+              </Link>
+            </li>
+          ))}
+          <li className="md:ml-3 md:my-0 my-7">
+            <button
+              className="h-12 w-12 rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-gray-700"
+              onClick={() => {
+                setTheme((prevTheme: string) => (prevTheme === "light" ? "dark" : "light"));
+              }}>
+              <svg
+                className={`fill-violet-700 block ${theme === "light" ? "hidden" : ""}`}
+                fill="currentColor"
+                viewBox="0 0 20 20">
+                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+              </svg>
+              <svg
+                className={`fill-yellow-500 block ${theme === "dark" ? "hidden" : ""}`}
+                fill="currentColor"
+                viewBox="0 0 20 20">
+                <path
+                  d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                  fillRule="evenodd" clipRule="evenodd">
+                </path>
+              </svg>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
