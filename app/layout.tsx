c30f1bf348fbaeba6b029d83c0c4e5e7ed719a7e
victorzarzar <victorzarzar58@gmail.com>
2024-01-03 08:38:49 -0300

    corrections in positioned navbar.tsx and footer.tsx in project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,43 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+'use client';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Navbar from './components/Navbar';
+import { Footer } from './components/Footer';
+import { useState } from "react";
+import { ThemeProvider, createGlobalStyle, DefaultTheme } from "styled-components";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'Portfolio',
-  description: 'Portfolio Victor Zarzar TS - NextJS',
+interface ThemeProps {
+  body: string;
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const GlobalStyle = createGlobalStyle<{ theme: DefaultTheme }>`
+  body {
+    background: ${({ theme }) => theme.body};
+  }`;
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+  const [theme, setTheme] = useState("light");
+
+  const light: ThemeProps = {
+    body: "#E2E2E2"
+  };
+  const dark: ThemeProps = {
+    body: "#111827"
+  };
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider theme={theme === "light" ? dark : light}>
+          <GlobalStyle theme={theme === "light" ? dark : light} />
+          <Navbar theme={theme} setTheme={setTheme} />
+          <main>{children}</main>
+          <Footer />
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,21 @@
-'use client';
-import { Footer } from "./components/Footer";
-import { useState } from "react";
-import { ThemeProvider, createGlobalStyle, DefaultTheme } from "styled-components";
-import Navbar from "./components/Navbar";
+import { Metadata } from "next";
 import Home from "./pages/Home";
 import Location from "./pages/Location";
 import Skills from "./pages/Skills";
-interface ThemeProps {
-  body: string;
-}
 
-const GlobalStyle = createGlobalStyle<{ theme: DefaultTheme }>`
-  body {
-    background: ${({ theme }) => theme.body};
-  }`;
+export const metadata: Metadata = {
+  title: 'Portfolio',
+  description: 'Portfolio Victor Zarzar TS - NextJS',
+};
 
 export default function App() {
 
-  const [theme, setTheme] = useState("light");
-
-  const light: ThemeProps = {
-    body: "#E2E2E2"
-  };
-  const dark: ThemeProps = {
-    body: "#111827"
-  };
-
   return (
     <main>
-      <ThemeProvider theme={theme === "light" ? dark : light}>
-        <GlobalStyle theme={theme === "light" ? dark : light} />
-        <Navbar theme={theme} setTheme={setTheme} />
+      <link rel="shortcut icon" href="./favicon.ico"></link>
         <Home />
         <Skills />
         <Location />
-        <Footer />
-      </ThemeProvider>
     </main>
   )
 }
